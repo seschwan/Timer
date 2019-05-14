e441fea7966e62650e4f1d76a1d0fb4fc277c9a5
Seschwan <seschwanssd@Coreys-iMac.local>
2019-05-13 19:35:53 -0600

    UI Setup

---
+++ Timer/Timer/Model/CustomButton.swift
@@ -0,0 +1,30 @@
+//
+//  CustomButton.swift
+//  Timer
+//
+//  Created by Seschwan on 5/13/19.
+//  Copyright © 2019 Seschwan. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
